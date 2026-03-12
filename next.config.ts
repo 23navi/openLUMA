d02444862675426fae297b0f658bc1b587dafea8
23navi <navisureka23@gmail.com>
2026-03-12 18:53:11 +0800

    chore: add transpile packages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: process.env.VERCEL ? undefined : "standalone",
+  transpilePackages: ["mathml2omml", "pptxgenjs"],
   serverExternalPackages: [],
   experimental: {
     proxyClientMaxBodySize: '200mb',
